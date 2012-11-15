8045cd488aa83dbc2fe0c58ab3f398c69b282957
David Jones <unixmonkey1@gmail.com>
2012-11-14 20:07:51 -0500

    has layout

--- darkroast.rb
+++ darkroast.rb
@@ -4,8 +4,15 @@ class App < E
 
   map '/'
 
+  layout :master
+  format :html
+  #
+  setup '.html' do
+    engine :ERB
+  end
+
   def index
-    # ...
-    "<html><head></head><body>foobar</body></html>"
+    render
+    # "<html><head></head><body><h1>Dark Roast</h1><p>Classy Links</p></body></html>"
   end
 end
