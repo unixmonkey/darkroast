673ad0410cadab1e737565d20ff1e1d57ff33d7d
David Jones <unixmonkey1@gmail.com>
2012-11-14 20:46:30 -0500

    Posts

--- darkroast.rb
+++ darkroast.rb
@@ -8,6 +8,8 @@ class App < E
   layout :layout
 
   def index
+    @posts = HTTParty.get('http://www.reddit.com/r/programming.json').
+      parsed_response['data']['children']
     render
   end
 end
