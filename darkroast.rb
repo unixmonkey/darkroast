e0a4b3bf1d135e8aa0b68779a4df92d3dd32b87b
David Jones <unixmonkey1@gmail.com>
2012-11-14 20:12:33 -0500

    Layout works better

--- darkroast.rb
+++ darkroast.rb
@@ -4,15 +4,9 @@ class App < E
 
   map '/'
 
-  layout :master
-  format :html
-  #
-  setup '.html' do
-    engine :ERB
-  end
+  layout :layout
 
   def index
     render
-    # "<html><head></head><body><h1>Dark Roast</h1><p>Classy Links</p></body></html>"
   end
 end
